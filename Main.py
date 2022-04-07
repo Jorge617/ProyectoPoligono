6b49706709bda2177ec57e2912d7968976a34928
juandyvalverde <juandevh@gmail.com>
2022-04-07 19:18:32 +0200

    Añadir archivos concavidad

---
+++ Main.py
@@ -0,0 +1,16 @@
+print("Bienvenido a nuestra aplicacion de poligonos\n")
+print("Pulsa 1 para calcular la concavidad" )
+print("Pulsa 2 para calcular la convexidad")
+print("Pulsa 3 para calcular el perimetro")
+print("Pulsa 4 para saber si esta dentro \n")
+mathop = input("Elija una opcion: \n") # Ask for the choice and make sure it is an interger.
+if mathop == "1":
+  
+elif mathop == "2":
+ 
+elif mathop == "3":
+   
+elif mathop == "4":
+   
+else:
+    print("Please enter a valid option. Exiting")
---
+++ Poligono.py
@@ -0,0 +1,9 @@
+class Poligono:
+      def __init__(self,n_lados,m_lados): 
+        self.numero_lados= n_lados;
+        self.medida_lado= m_lados;
+        
+        
+      def perimetro(self):
+        self.perimetro= numero_lados * medida_lado
+     
